38f1f6ace332a258dd7296019958e92ec6649686
jacobPDhardy <145456509+jacobPDhardy@users.noreply.github.com>
2024-06-10 15:12:24 +0100

    Add project files.

---
+++ Apple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searchy
+{
+    internal class Apple
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Apple(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Searchy;
+
+static List<T> SuperCoolSearch<T>(List<T> objectList, string search)
+{
+    string searchLowerCase = search.ToLower();
+    const int notFound = -1;
+    List<T> filteredObjectList = new();
+    foreach (var @object in objectList)
+    {
+        if (@object is not null)
+        {
+            foreach (var property in @object.GetType().GetProperties())
+            {
+                object? propertyValue = property.GetValue(@object);
+                if (propertyValue is not null)
+                {
+                    string? propertyValueString = propertyValue.ToString();
+                    if (propertyValueString is not null && propertyValueString.ToLower().IndexOf(searchLowerCase) is not notFound)
+                    {
+                        filteredObjectList.Add(@object);                       
+                    }
+                }
+            }
+        }
+    }
+    return filteredObjectList;
+}
+
+List<Apple> apples = new List<Apple>() { new("Pink lady","The best"),new("Green apple","mid"), new("Normal apple","It is quite yummy"), new("Pineapple", "Incredibly yummy") };
+
+List<Apple> yummyApples = SuperCoolSearch(apples,"yummy");
+Console.WriteLine(string.Join(",",yummyApples.Select(yummyApple => yummyApple.Name).ToArray()));
